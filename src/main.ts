cc9bf7b794722d6ca8dff8017115adf04b6da952
feryirawan <ir15y4hh@gmail.com>
2025-03-03 12:30:51 +0700

    sss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { mount } from 'svelte'
-import './app.css'
-import App from './App.svelte'
+import './assets/index.css'
+import App from './lib/App.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app')!,
