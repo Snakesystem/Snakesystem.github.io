b208b6acfb06df4f5831a2b54b08b53d50efc731
Snakesystem <ir15y4hh@gmail.com>
2025-05-03 18:48:16 +0700

    Update logo

--- src/routes/Resume.svelte
+++ src/routes/Resume.svelte
@@ -14,29 +14,31 @@
           <h3 class="resume-title">Sumary</h3>
 
           <div class="resume-item pb-0">
-            <h4>Brandon Johnson</h4>
-            <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
+            <h4>Feri Irawansyah</h4>
+            <p><em>I'm a Freelancer, Programmer, and Software Engineer with 4+ years of experience in web development.</em></p>
             <ul>
-              <li>Portland par 127,Orlando, FL</li>
-              <li>(123) 456-7891</li>
-              <li>alice.barkley@example.com</li>
+              <li>Gg. Haji Mumun no 5, Kota Depok</li>
+              <li>(+62) 82323443535</li>
+              <li>feryirawansyah09@gmail.com</li>
             </ul>
-          </div><!-- Edn Resume Item -->
+          </div>
 
           <h3 class="resume-title">Education</h3>
+          
           <div class="resume-item">
-            <h4>Master of Fine Arts &amp; Graphic Design</h4>
-            <h5>2015 - 2016</h5>
-            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-            <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
-          </div><!-- Edn Resume Item -->
+            <h4>Bachelor of Accounting &amp; Finance</h4>
+            <h5>2019 - 2023</h5>
+            <p><em>Universitas Pamulang, Tangerang, Indonesia</em></p>
+            <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
+          </div>
 
           <div class="resume-item">
-            <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-            <h5>2010 - 2014</h5>
-            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-            <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
-          </div><!-- Edn Resume Item -->
+            <h4>Vocational High School</h4>
+            <h5>2016 - 2019</h5>
+            <p><em>SMK Miftahul Ulum, Jawa Tengah, Indonesia</em></p>
+            <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
+          </div>
+
 
         </div>
 
